a771080a9c6b9189e51c2f70f9ac3dd4ba38280f
Siyue Zhang <sz4651@log-3.hpc.nyu.edu>
2023-12-27 12:08:24 -0500

    test

--- run/squall_selector_train.sh
+++ run/squall_selector_train.sh
@@ -3,9 +3,9 @@ export WANDB_PROJECT=SynTableQA_Selector
 export WANDB_ENTITY=siyue-zhang
 
 model_name="neulab/omnitab-large"
-run_name="squall_selector_omnitab_aug"
+run_name="squall_selector_omnitab_aug_3k"
 dataset_name="squall"
-output_dir="output/squall_selector_omnitab_aug"
+output_dir="output/squall_selector_omnitab_aug_3k"
 
 python ./train.py \
   --do_train \
